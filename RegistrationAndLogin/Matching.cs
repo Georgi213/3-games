27718ac0ccfb304b16b8b319541be147f7168ada
Georgi213 <77331773+Georgi213@users.noreply.github.com>
2022-10-05 11:23:37 +0300

    Add files via upload

---
+++ RegistrationAndLogin/Matching.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistrationAndLogin
+{
+    public partial class Matching : Form
+    {
+        Random rnd = new Random();
+        TableLayoutPanel tableLayoutPanel;
+        Label firstClicked = null;
+        Label secondClicked = null;
+        Timer timer1 = new Timer { Interval = 750 };
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+        public Matching()
+        {
+            CenterToScreen();
+            timer1.Tick += timer1_Tick;
+            Text = "Matching game";
+            ClientSize = new Size(550, 550);
+            tableLayoutPanel = new TableLayoutPanel
+            {
+                BackColor = Color.White,
+                Dock = DockStyle.Fill,
+                CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset,
+                RowCount = 4,
+                ColumnCount = 4
+            };
+
+            this.Controls.Add(tableLayoutPanel);
+            for (int i = 0; i < 4; i++)
+            {
+                tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+                tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+                for (int j = 0; j < 4; j++)
+                {
+
+                    Label lbl = new Label
+                    {
+                        BackColor = Color.LightBlue,
+                        AutoSize = false,
+                        Dock = DockStyle.Fill,
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Font = new Font("Webdings", 45, FontStyle.Bold),
+                    };
+
+
+                    tableLayoutPanel.Controls.Add(lbl, i, j);
+                };
+
+            }
+            foreach (Control control in tableLayoutPanel.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = rnd.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    icons.RemoveAt(randomNumber);
+                }
+                iconLabel.ForeColor = iconLabel.BackColor;
+                iconLabel.Click += label1_Click;
+            }
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+                timer1.Start();
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (firstClicked.Text == secondClicked.Text)
+            {
+                firstClicked.ForeColor = firstClicked.ForeColor;
+                secondClicked.ForeColor = secondClicked.ForeColor;
+            }
+            else
+            {
+                firstClicked.ForeColor = firstClicked.BackColor;
+                secondClicked.ForeColor = secondClicked.BackColor;
+            }
+            firstClicked = null;
+            secondClicked = null;
+            timer1.Stop();
+            CheckForWinner();
+        }
+        private void CheckForWinner()
+        {
+            foreach (Control control in tableLayoutPanel.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            MessageBox.Show("Sa sobitasid kõik ikoonid!", "Palju õnne");
+            Close();
+        }
+    }
+}
